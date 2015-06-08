8ff64921d5130cb893128842536a84fee3d407b0
Jordi Orlando <jordi.orlando@gmail.com>
2015-06-07 20:44:33 -0500

    Update license

--- keyboard/ion60/keymap.c
+++ keyboard/ion60/keymap.c
@@ -1,3 +1,20 @@
+/*
+Copyright 2015 Jordi Pakey-Rodriguez <jordi.orlando@d1sc0te.ch>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -49,8 +66,8 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 6: Media */
     [6] = KEYMAP(
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,SLEP,TRNS,MPRV,MPLY,MNXT,MUTE,VOLD,VOLU,
-        NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,TRNS,TRNS,TRNS,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,
-        NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,TRNS,TRNS,TRNS,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,
+        NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,TRNS,WREF,TRNS,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,
+        NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,WBAK,WSTP,WFWD,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,
         NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,TRNS,TRNS,TRNS,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,
         TRNS  ,TRNS   ,TRNS  ,TRNS   ,TRNS  ,TRNS   ,TRNS  ,TRNS   ,TRNS  ,TRNS  ),
 };
