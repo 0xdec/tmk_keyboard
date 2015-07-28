28fb80c3569734705f73c677027ee1ba5a371b0d
Jordi Orlando <jordi.orlando@gmail.com>
2015-07-27 19:57:22 -0500

    Disable software debouncing

--- keyboard/ion60/config.h
+++ keyboard/ion60/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define MATRIX_HAS_GHOST
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5 // TODO: hardware debouncing
+#define DEBOUNCE 0
 
 /* Key combination for command */
 #define IS_COMMAND() ( \
