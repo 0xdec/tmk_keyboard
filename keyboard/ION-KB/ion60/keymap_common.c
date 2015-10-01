228ef5bbdcaa609bd2ce953f237c18dc528e6077
Jordi Orlando <jordi.orlando@gmail.com>
2015-10-01 05:47:31 -0500

    Remove unused reset code (BTLD is now in core)

--- keyboard/ION-KB/ion60/keymap_common.c
+++ keyboard/ION-KB/ion60/keymap_common.c
@@ -21,12 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* translates key to keycode */
 uint8_t keymap_key_to_keycode(uint8_t layer, keypos_t key) {
-    /*uint8_t keycode = pgm_read_byte(&keymaps[(layer)][(key.row)][(key.col)]);
-
-    if (keycode == KC_RST) {
-        bootloader_jump();
-    }*/
-
     return pgm_read_byte(&keymaps[(layer)][(key.row)][(key.col)]);
 }
 
